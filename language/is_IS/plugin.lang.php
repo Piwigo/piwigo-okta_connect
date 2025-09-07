5079e2ba2f32a96af2510455647795675a45041f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-07 06:57:10 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['How to configure okta'] = 'Hvernig á að stilla okta';
+$lang['IDP Entity ID'] = 'IDP aðila auðkenni';
+$lang['Identity provider'] = 'Auðkennisveitandi';
+$lang['If no roles are configured, users will be have guest status'] = 'Ef engin hlutverk eru stillt munu notendur hafa stöðuna gestur';
+$lang['In okta you must configure the Service Provider entityId as <i>"https://yourPiwigoURL/piwigo/plugins/okta_connect/metadata.php"</i>'] = 'Í okta verður þú að stilla þjónustuveituauðkennið sem <i>"https://yourPiwigoURL/piwigo/plugins/okta_connect/metadata.php"</i>';
+$lang['Keep alternate access to the standard Piwigo identification page'] = 'Halda aðgangi að stöðluðu auðkenningarsíðu Piwigo';
+$lang['OKTA Authentication'] = 'OKTA Auðkenning';
+$lang['SSO URL'] = 'SSO URL';
+$lang['Sign in using Piwigo'] = 'Innskráning með Piwigo';
+$lang['The service provider Assertion Consumer Service should be <i>"https://yourPiwigoURL/piwigo/identification.php?okta_sso=tryLoginPiwigo"</i>'] = 'Þjónustuveitan fyrir fullyrðingarþjónustu fyrir neytendur ætti að vera <i>"https://yourPiwigoURL/piwigo/identification.php?okta_sso=tryLoginPiwigo"</i>';
+$lang['X.509 Certificate'] = 'X.509 Skírteini';
