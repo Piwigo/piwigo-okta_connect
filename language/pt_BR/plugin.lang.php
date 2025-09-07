0952a784372c7277571602d9af99b9a151291394
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-07 06:57:13 +0200

    Update pt_BR, thanks to flaviove

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['How to configure okta'] = 'Como configurar o okta';
+$lang['IDP Entity ID'] = 'ID da entidade IDP';
+$lang['Identity provider'] = 'Provedor de identidade';
+$lang['If no roles are configured, users will be have guest status'] = 'Se nenhuma função for configurada, os usuários terão status de convidado';
+$lang['In okta you must configure the Service Provider entityId as <i>"https://yourPiwigoURL/piwigo/plugins/okta_connect/metadata.php"</i>'] = 'No okta você deve configurar o entityId do Provedor de Serviço como <i>"https://yourPiwigoURL/piwigo/plugins/okta_connect/metadata.php"</i>';
+$lang['Keep alternate access to the standard Piwigo identification page'] = 'Mantenha acesso alternativo à página de identificação padrão do Piwigo';
+$lang['OKTA Authentication'] = 'Autenticação OKTA';
+$lang['SSO URL'] = 'SSO URL';
+$lang['Sign in using Piwigo'] = 'Faça login usando o Piwigo';
+$lang['The service provider Assertion Consumer Service should be <i>"https://yourPiwigoURL/piwigo/identification.php?okta_sso=tryLoginPiwigo"</i>'] = 'O provedor de serviços Assertion Consumer Service deve ser <i>"https://yourPiwigoURL/piwigo/identification.php?okta_sso=tryLoginPiwigo"</i>';
+$lang['X.509 Certificate'] = 'Certificado X.509';
