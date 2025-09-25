3db4ab23f9c7e77d08371e8e9b0d6c809603e11a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2025-09-25 13:44:04 +0200

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['In okta you must configure the Service Provider entityId as <i>"https://yourPiwigoURL/piwigo/plugins/okta_connect/metadata.php"</i>'] = 'Okta\'da Servis Sağlayıcısı varlık kimliğini <i>"https://yourPiwigoURL/piwigo/plugins/okta_connect/metadata.php"</i> şeklinde yapılandırmanız gerekir.';
+$lang['The service provider Assertion Consumer Service should be <i>"https://yourPiwigoURL/piwigo/identification.php?okta_sso=tryLoginPiwigo"</i>'] = 'Hizmet sağlayıcısı Assertion Tüketici Hizmeti <i>"https://yourPiwigoURL/piwigo/identification.php?okta_sso=tryLoginPiwigo"</i> olmalıdır';
+$lang['Keep alternate access to the standard Piwigo identification page'] = 'Standart Piwigo tanımlama sayfasına alternatif erişimi koruyun';
+$lang['X.509 Certificate'] = 'X.509 Sertifikası';
+$lang['Sign in using Piwigo'] = 'Piwigo kullanarak oturum açın';
+$lang['SSO URL'] = 'SSO URL';
+$lang['OKTA Authentication'] = 'OKTA Kimlik Doğrulaması';
+$lang['If no roles are configured, users will be have guest status'] = 'Hiçbir rol yapılandırılmazsa, kullanıcılar misafir statüsüne sahip olacaktır';
+$lang['Identity provider'] = 'Kimlik sağlayıcı';
+$lang['IDP Entity ID'] = 'IDP Kimliği';
+$lang['How to configure okta'] = 'Okta nasıl yapılandırılır?';
