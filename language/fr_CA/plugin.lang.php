fae2fa003e70c3c66c675771fa11ff946ace53a6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-17 19:00:25 +0200

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['In okta you must configure the Service Provider entityId as <i>"https://yourPiwigoURL/piwigo/plugins/okta_connect/metadata.php"</i>'] = 'Dans Okta, vous devez configurer l\'entityId du fournisseur de services comme <i>"https://votreURLPiwigo/piwigo/plugins/okta_connect/metadata.php"</i>';
+$lang['The service provider Assertion Consumer Service should be <i>"https://yourPiwigoURL/piwigo/identification.php?okta_sso=tryLoginPiwigo"</i>'] = 'Le fournisseur de services Assertion Consumer Service doit être <i>"https://votreURLPiwigo/piwigo/identification.php?okta_sso=tryLoginPiwigo"</i>';
