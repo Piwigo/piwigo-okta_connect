9ba55f33f8e7dd93fe37862a5d9d00674bdbae9d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-05-18 20:44:54 +0200

    Update zh_TW, thanks to closer

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['How to configure okta'] = '如何設置 okta';
+$lang['IDP Entity ID'] = 'IDP Entity ID';
+$lang['Identity provider'] = '身份識別提供者';
+$lang['If no roles are configured, users will be have guest status'] = '如果沒有設置角色 (role)，那麼使用者就會被設定為訪客狀態';
+$lang['In okta you must configure the Service Provider entityId as <i>"https://yourPiwigoURL/piwigo/plugins/okta_connect/metadata.php"</i>'] = '在 okta 中，您必須依照 <i>"https://yourPiwigoURL/piwigo/plugins/okta_connect/metadata.php"</i> 的說明，設置服務提供者的 entityId。';
+$lang['Keep alternate access to the standard Piwigo identification page'] = '保留額外存取 Piwigo 標準認證頁面的方式';
+$lang['OKTA Authentication'] = 'OKTA 驗證';
+$lang['SSO URL'] = 'SSO 網址';
+$lang['Sign in using Piwigo'] = '使用 Piwigo 登入';
+$lang['The service provider Assertion Consumer Service should be <i>"https://yourPiwigoURL/piwigo/identification.php?okta_sso=tryLoginPiwigo"</i>'] = '服務提供者 Assertion Consumer Service 應為 <i>"https://yourPiwigoURL/piwigo/identification.php?okta_sso=tryLoginPiwigo"</i>';
+$lang['X.509 Certificate'] = 'X.509 認證';
